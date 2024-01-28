3b430fb84fa74c1c43136c4fa1089027fb04c4ef
Ericka2306 <114937300+Ericka2306@users.noreply.github.com>
2024-01-28 21:38:22 +0300

    Update VenteJPA.java

--- Voiture/src/main/java/com/example/ventevoiture01/Repository/VenteJPA.java
+++ Voiture/src/main/java/com/example/ventevoiture01/Repository/VenteJPA.java
@@ -10,11 +10,9 @@ import com.example.ventevoiture01.Models.Vente;
 
 @Repository
 public interface VenteJPA extends JpaRepository<Vente, Integer> {
-    
     @Query(value = "SELECT id_vendeur, COUNT(*) AS nombre_ventes " +
             "FROM vente " +
             "GROUP BY id_vendeur " +
             "ORDER BY nombre_ventes DESC", nativeQuery = true)
-    List<Vente> countVentesParVendeur();
-    
+    List<Object[]> countVentesParVendeur();
 }
