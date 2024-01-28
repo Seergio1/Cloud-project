29c134e096aeac8685af56ef033b45a6ce0c21a4
Seergio1 <119038649+Seergio1@users.noreply.github.com>
2024-01-28 20:10:39 +0300

    Update cors 2

--- Voiture/src/main/java/com/example/ventevoiture01/Application.java
+++ Voiture/src/main/java/com/example/ventevoiture01/Application.java
@@ -1,5 +1,6 @@
 package com.example.ventevoiture01;
 
+import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
