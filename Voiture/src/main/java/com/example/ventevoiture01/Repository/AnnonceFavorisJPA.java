59098c56b6d8017763496ac16b06370187bf437a
Ericka2306 <114937300+Ericka2306@users.noreply.github.com>
2024-01-28 22:35:52 +0300

    Update AnnonceFavorisJPA.java

--- Voiture/src/main/java/com/example/ventevoiture01/Repository/AnnonceFavorisJPA.java
+++ Voiture/src/main/java/com/example/ventevoiture01/Repository/AnnonceFavorisJPA.java
@@ -4,8 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.ventevoiture01.Models.Annonce_Favoris;
+import com.example.ventevoiture01.Models.MeilleureAnnonce;
 
 @Repository
 public interface AnnonceFavorisJPA extends JpaRepository<Annonce_Favoris, Integer> {
-
+  @Query(value = "SELECT a, COUNT(*) AS favorisCount " +
+            "FROM AnnonceFavoris af " +
+            "JOIN af.annonce a " +
+            "GROUP BY a " +
+            "ORDER BY favorisCount DESC")
+    List<MeilleureAnnonce> countFavorisByAnnonce();
 }
