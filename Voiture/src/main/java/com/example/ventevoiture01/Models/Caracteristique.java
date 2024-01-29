15566b8b75eb492c4a8618819a435624e8baddc5
Ericka2306 <114937300+Ericka2306@users.noreply.github.com>
2024-01-29 06:40:35 +0300

    Update Caracteristique.java

--- Voiture/src/main/java/com/example/ventevoiture01/Models/Caracteristique.java
+++ Voiture/src/main/java/com/example/ventevoiture01/Models/Caracteristique.java
@@ -1,6 +1,8 @@
 package com.example.ventevoiture01.Models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
