dd63caa81fe4e0ecd2f9ee4bd3eed52f5d2d57a5
Steel Soul <120133470+P15B-Y32-ETU1951@users.noreply.github.com>
2024-02-10 01:09:17 +0300

    Update AnnonceFavorisJPA.java

--- Voiture/src/main/java/com/example/ventevoiture01/Repository/AnnonceFavorisJPA.java
+++ Voiture/src/main/java/com/example/ventevoiture01/Repository/AnnonceFavorisJPA.java
@@ -1,24 +1,31 @@
 package com.example.ventevoiture01.Repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.example.ventevoiture01.Models.Annonce_Favoris;
-import com.example.ventevoiture01.Models.MeilleureAnnonce;
+import java.time.LocalDateTime;
 
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-import java.util.List;
+import com.example.ventevoiture01.Models.Annonce;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-public interface AnnonceFavorisJPA extends JpaRepository<Annonce_Favoris, Integer> {
-  @Query(value = "SELECT a, COUNT(*) AS favorisCount " +
-      "FROM Annonce_Favoris af " +
-      "JOIN af.annonce a " +
-      "GROUP BY a " +
-      "ORDER BY favorisCount DESC LIMIT 3")
-  List<Object[]> countFavorisByAnnonce();
+public interface AnnonceJPA extends JpaRepository<Annonce, Integer> {
+  @Modifying
+  @Transactional
+  @Query("UPDATE Annonce a SET a.etat_annonce = '1' WHERE a.id_annonce = :id")
+  void valider(@Param("id") int id);
+
+  @Modifying
+  @Transactional
+  @Query("UPDATE Annonce a SET a.validation_annonce = :date WHERE a.id_annonce = :id")
+  void date_validation(@Param("id") int id, @Param("date") LocalDateTime date);
+
+  Page<Annonce> findAll(Pageable pageable);
 
 }
